2f3c3bff76d115436b54fb676011027b4ad6cdd2
Kevin Kayser <kkayser@rpionline.com>
2019-10-02 13:12:26 -0400

    adding scss and a fw other modules.

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,11 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styles from './header.module.scss'
+
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header  className={styles.headerColor}>
     <div
       style={{
         margin: `0 auto`,
